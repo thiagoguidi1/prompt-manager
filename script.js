9f787c031162081120f5fd8ce19740115183a715
thiagoguidi1 <thiagoguidi@outlook.com>
2025-10-10 01:23:40 -0300

    start prompts list logic :wrench:

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Chave para identificar os dados salvos pela aplicação no localStorage
+const STORAGE_KEY = 'prompts_storage';
+
+//Estado carregar os prompts salvos e exibir
+const state = {
+  prompts: [],
+  selectedId: null,
+}
+
 // Seleciona os elementos por id
 const elements = {
   promptTitle: document.getElementById('prompt-title'),
@@ -7,6 +16,7 @@ const elements = {
   btnOpen: document.getElementById('btn-open'),
   btnCollapse: document.getElementById('btn-collapse'),
   sidebar: document.querySelector('.sidebar'),
+  btnSave: document.getElementById('btn-save'),
 };
 
 // Atualiza o estado do wrapper conforme o conteúdo do elemento
@@ -52,8 +62,62 @@ function attachSidebarHandlers() {
   elements.btnOpen.style.display = 'none';
 }
 
+function save() {
+  //innerHTML para manter formatação
+  //textContent para manter apenas o texto
+  const title = elements.promptTitle.innerHTML.trim();
+  const content = elements.promptContent.innerHTML.trim();
+  const hasContent = elements.promptContent.textContent.trim()
+
+  if (!title || !hasContent) {
+    alert('Título e conteúdo não podem estar vazios.');
+    return;
+  }
+
+  if (state.selectedId) {
+    // Edityando um prompt existente
+  } else {
+    // Criando um novo prompt
+    const newPrompt = {
+      // Gera um ID simples baseado no timestamp
+      id: Date.now().toString(36),
+      title,
+      content,
+    };
+    state.prompts.unshift(newPrompt);
+    state.selectedId = newPrompt.id;
+  }
+
+  persist();
+}
+
+function persist() {
+  try {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(state.prompts));
+    alert('Prompt salvo com sucesso!');
+  } catch (error) {
+    console.error('Erro ao salvar os prompts:', error);
+  }
+}
+
+function load() {
+  try {
+    const storage = localStorage.getItem(STORAGE_KEY);
+    state.prompts = storage ? JSON.parse(storage) : [];
+    selectedId = null;
+
+    console.log('Prompts carregados:', state.prompts);
+  } catch (error) {
+    console.error('Erro ao carregar os prompts:', error);
+  }
+}
+
+//Eventos dos botoes
+elements.btnSave.addEventListener('click', save);
+
 // Função de inicialização
 function init() {
+  load();
   attachAllEditableHandlers();
   attachSidebarHandlers();
 }
